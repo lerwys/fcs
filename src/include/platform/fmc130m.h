c7b7937e315ab0eebe27360364b4ff705ebd2f9b
Lucas Russo <lerwys@gmail.com>
2013-06-21 08:16:35 -0300

    src/*/fmc130m.h: fix delay values for ml605 kit

--- src/include/platform/fmc130m.h
+++ src/include/platform/fmc130m.h
@@ -8,15 +8,15 @@
 
 #include "common.h"
 
-// adc0 ? -> IDELAY_TAP(7)
-// adc1 ? -> IDELAY_TAP(7)
-// adc2 ? -> IDELAY_TAP(7)
-// adc3 ? -> IDELAY_TAP(7)
+// adc0 -1.780/0.078 -> IDELAY_TAP(23)
+// adc1 -1.947/0.078 -> IDELAY_TAP(25)
+// adc2 -0.786/0.078 -> IDELAY_TAP(10)
+// adc3 -1.509/0.078 -> IDELAY_TAP(20)
 const struct delay_lines fmc_130m_ml605_delay_l[] = {
-  {DELAY_LINES_INIT,  7},
-  {DELAY_LINES_INIT,  7},
-  {DELAY_LINES_INIT,  7},
-  {DELAY_LINES_INIT,  7},
+  {DELAY_LINES_INIT,  23+6},
+  {DELAY_LINES_INIT,  25+6},
+  {DELAY_LINES_INIT,  11+6},
+  {DELAY_LINES_INIT,  20+6},
   {DELAY_LINES_END,  -1}
 };
 

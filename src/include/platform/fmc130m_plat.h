52a4b00bf19e38e3286f457e25874d41a1365d90
Lucas Russo <lerwys@gmail.com>
2014-03-19 08:50:23 -0300

    update ADC delays

--- src/include/platform/fmc130m_plat.h
+++ src/include/platform/fmc130m_plat.h
@@ -25,18 +25,18 @@
 
 const struct delay_lines fmc_130m_ml605_delay_data_l[] = {
   {DELAY_LINES_INIT,  31},
+  {DELAY_LINES_INIT,  10},
   {DELAY_LINES_INIT,  0},
-  {DELAY_LINES_INIT,  31},
-  {DELAY_LINES_INIT,  31},
+  {DELAY_LINES_INIT,  21},
   {DELAY_LINES_END,  -1}
 };
 
 // clk delay
 const struct delay_lines fmc_130m_ml605_delay_clk_l[] = {
   {DELAY_LINES_INIT,  31},
+  {DELAY_LINES_INIT,  10},
   {DELAY_LINES_INIT,  0},
-  {DELAY_LINES_INIT,  31},
-  {DELAY_LINES_INIT,  31},
+  {DELAY_LINES_INIT,  21},
   {DELAY_LINES_END,  -1}
 };
 
